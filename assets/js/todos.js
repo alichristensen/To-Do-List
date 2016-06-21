4a6f3ba1c94e236f1a6e752a79fee077ce5c6c22
alichristensen <ali.christensen1@gmail.com>
2016-06-21 12:12:40 -0500

    adding all edits

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,7 +1,26 @@
 //check off specific todos by clicking
-$("li").click(function() {
-	$(this).css({
-		color: "gray", 
-		textDecoration: "line-through"
-	});
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
 });
+
+//click on X to delete todo
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	}); 
+	event.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(event) {
+	if (event.which === 13){
+		//grab todo text
+		var todoText = $(this).val(); 
+		$(this).val("");
+		//create new li and add todo text
+		$("ul").append("<li><span><id class='fa fa-trash'></id></span> " + todoText + "</li>");
+	}
+});
+
+$(".fa-plus").click(function() {
+	$("input[type='text'").fadeToggle();
+})
